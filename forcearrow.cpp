9083f6e51284885393458ebe9e2ed2d2bf3e07e4
Thomas Leese <inbox@thomasleese.me>
2016-03-03 13:26:17 +0000

    Improve force arrow coding style

--- forcearrow.cpp
+++ forcearrow.cpp
@@ -20,36 +20,39 @@ ForceArrow::ForceArrow(QColor colour, float scale, QObject *parent) :
 
 }
 
-ForceArrow::~ForceArrow() {
-    delete m_entity;
+ForceArrow::~ForceArrow()
+{
+
 }
 
-void ForceArrow::addToScene(Qt3D::QEntity *scene) {
-    m_entity = new Qt3D::QEntity(scene);
+void ForceArrow::addToScene(Qt3D::QEntity *scene)
+{
+    auto entity = new Qt3D::QEntity(scene);
 
-    auto material = new Qt3D::QPhongMaterial(scene);
+    auto material = new Qt3D::QPhongMaterial(entity);
     material->setAmbient(m_colour);
-    m_entity->addComponent(material);
+    entity->addComponent(material);
 
-    auto mesh = new Qt3D::QMesh;
+    auto mesh = new Qt3D::QMesh(entity);
     mesh->setSource(QUrl("qrc:/models/arrow.obj"));
-    m_entity->addComponent(mesh);
+    entity->addComponent(mesh);
 
-    auto transform = new Qt3D::QTransform;
+    auto transform = new Qt3D::QTransform(entity);
 
-    m_rotateTransform = new Qt3D::QRotateTransform;
+    m_rotateTransform = new Qt3D::QRotateTransform(entity);
     transform->addTransform(m_rotateTransform);
 
-    m_scaleTransform = new Qt3D::QScaleTransform;
+    m_scaleTransform = new Qt3D::QScaleTransform(entity);
     transform->addTransform(m_scaleTransform);
 
-    m_translateTransform = new Qt3D::QTranslateTransform;
+    m_translateTransform = new Qt3D::QTranslateTransform(entity);
     transform->addTransform(m_translateTransform);
 
-    m_entity->addComponent(transform);
+    entity->addComponent(transform);
 }
 
-void ForceArrow::update(QVector3D force, QVector3D position) {
+void ForceArrow::update(QVector3D force, QVector3D position)
+{
     QVector3D up = QVector3D(0, 1, 0);
     QVector3D dir = force.normalized();
 
@@ -63,11 +66,33 @@ void ForceArrow::update(QVector3D force, QVector3D position) {
     m_rotateTransform->setAngleDeg(angle);
 
     m_translateTransform->setTranslation(position);
+
     m_scaleTransform->setScale((force.length() / 500.) * m_scale);
 }
 
-void ForceArrow::update(btVector3 force, btVector3 position) {
+void ForceArrow::update(btVector3 force, btVector3 position)
+{
     QVector3D forceVector(force.x(), force.y(), force.z());
     QVector3D positionVector(position.x(), position.y(), position.z());
     update(forceVector, positionVector);
+}
+
+QColor ForceArrow::colour() const
+{
+    return m_colour;
+}
+
+void ForceArrow::setColour(const QColor &colour)
+{
+    m_colour = colour;
+}
+
+float ForceArrow::scale() const
+{
+    return m_scale;
+}
+
+void ForceArrow::setScale(float scale)
+{
+    m_scale = scale;
 }
--- forcearrow.h
+++ forcearrow.h
@@ -6,10 +6,10 @@
 #include <QVector3D>
 
 namespace Qt3D {
-    class QEntity;
-    class QRotateTransform;
-    class QTranslateTransform;
-    class QScaleTransform;
+class QEntity;
+class QRotateTransform;
+class QTranslateTransform;
+class QScaleTransform;
 }
 
 class btVector3;
@@ -23,14 +23,23 @@ public:
     ~ForceArrow();
 
     void addToScene(Qt3D::QEntity *scene);
+
     void update(QVector3D force, QVector3D position);
     void update(btVector3 force, btVector3 position);
+
+    QColor colour() const;
+    void setColour(const QColor &colour);
+
+    float scale() const;
+    void setScale(float scale);
+
+    Q_PROPERTY(QColor colour READ colour WRITE setColour)
+    Q_PROPERTY(float scale READ scale WRITE setScale)
 
 private:
     QColor m_colour;
     float m_scale;
 
-    Qt3D::QEntity *m_entity;
     Qt3D::QRotateTransform *m_rotateTransform;
     Qt3D::QTranslateTransform *m_translateTransform;
     Qt3D::QScaleTransform *m_scaleTransform;
